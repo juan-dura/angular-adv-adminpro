8faae997baf6d9bfa32534003d9682437aa7230b
Juan V. Dura <52622582+juan-dura@users.noreply.github.com>
2020-08-14 12:39:11 +0200

    Fin sección 4 - Router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../app-routing.module';
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+  ],
+  exports: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AppRoutingModule
+  ]
+})
+export class PagesModule { }
